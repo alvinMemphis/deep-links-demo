081f7601f4cbaf3f5959cf9c3d9c176f8233a899
alvinnsongambi9@gmail.com <alvinnsongambi9@gmail.com>
2023-11-08 20:43:24 +0300

    more links

---
+++ api/testlinks.js
@@ -0,0 +1,16 @@
+export const deeplinks =[
+ {name:"home", url:   "home",},
+ {name:"account", url:   "account",},
+{name:"showcases",  url:    "showcases/id/?page=12344" },
+{name:"portfolio",  url:   "p/7b7fb73e-1c4c-4e74-b470-ff14f363ad90&pageId=a67dddd3b1fc445ab94554fcf05ef2b2" },
+{ name:"notifications",  url:  "notifications?tab=personal" },
+{ name:"notfications with tabs", url:   "notifications?tab=business" },
+{   name:"appointments ",  url:"appointments" },
+{  name:"single appt", url:  "appointments/id" },
+{ name:"profile",  url:  "profile/id" },
+{   name:"single page ", url: "page/id/" },
+{   name:"policies", url: "page/policies" },
+{  name:"pay", url:  "page/pay" },
+{  name:"availability", url:  "page/availability" },
+{  name:"backstage", url:  "page/backstage" }
+]
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { useRouter } from 'next/router';
+import { deeplinks } from 'api/testlinks';
 
 
 
@@ -36,14 +37,13 @@ const redirecttoNativeApp =async (e,path='') => {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
- <div>
-      <button onClick={(e)=>redirecttoNativeApp(e,'account')}>Click to account</button>
-    </div>
 <div>
-      <button onClick={(e)=>redirecttoNativeApp(e,
-        'portfolio?mimbbossId=7b7fb73e-1c4c-4e74-b470-ff14f363ad90&pageId=a67dddd3b1fc445ab94554fcf05ef2b2')}>
-          Click to portfolio</button>
     </div>
+    {deeplinks.map(l=>(
+      <button style={{height:'30px',width:'120px',margin:'10px'}} onClick={(e)=>redirecttoNativeApp(e,
+        l.url)}>
+         {l.name}</button>
+    ))}
       </main>
 
       <Footer />
