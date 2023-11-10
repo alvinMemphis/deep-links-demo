b765507e0039049f9fe9844fb774314f55358021
alvinnsongambi9@gmail.com <alvinnsongambi9@gmail.com>
2023-11-10 17:52:07 +0300

    dd

--- api/testlinks.js
+++ api/testlinks.js
@@ -4,15 +4,15 @@ export const deeplinks =[
 {name:"showcases on page ",  url:    "showcases/2528b8e38dc24c069cb96bcebd18172f?page=85adeee87a7c44eeb0d17fc2705e8b6d" },
 {name:"showcases  general ",  url:    "home" },
 {name:" single showcase",  url:    "showcase/0bea53785f0e499a86cd3c069de42680" },
-{name:"portfolio",  url:   "p/7b7fb73e-1c4c-4e74-b470-ff14f363ad90&pageId=a67dddd3b1fc445ab94554fcf05ef2b2" },
+{name:"portfolio",  url:   "p/7b7fb73e-1c4c-4e74-b470-ff14f363ad90?pageId=a67dddd3b1fc445ab94554fcf05ef2b2" },
 { name:"notifications",  url:  "notifications?tab=personal" },
 { name:"notfications with tabs", url:   "notifications?tab=business" },
 {   name:"appointments ",  url:"appointments" },
 {  name:"single appt", url:  "appointments/0223ddf2615a43c0869ce4f46f43c924" },
-{ name:"profile",  url:  "profile/id" },
+{ name:"profile",  url:  "profile/7b7fb73e-1c4c-4e74-b470-ff14f363ad90" },
 {   name:"single page ", url: "page/85adeee87a7c44eeb0d17fc2705e8b6d" },
 {   name:"policies", url: "page/policies" },
 {  name:"pay", url:  "page/pay" },
-{  name:"availability", url:  "page/availability" },
-{  name:"backstage", url:  "page/backstage" }
+{  name:"availability", url:  "availability" },
+{  name:"backstage", url:  "backstage" }
 ]
