b1e0b618bbec1dfbc5b72957affc583750af556e
alvinnsongambi9@gmail.com <alvinnsongambi9@gmail.com>
2023-11-11 11:45:04 +0300

    more urls

--- api/testlinks.js
+++ api/testlinks.js
@@ -3,8 +3,9 @@ export const deeplinks =[
  {name:"account", url:   "account",},
 {name:"showcases on page ",  url:    "showcases/2528b8e38dc24c069cb96bcebd18172f?page=85adeee87a7c44eeb0d17fc2705e8b6d" },
 {name:"showcases  general ",  url:    "home" },
-{name:" single showcase",  url:    "showcase/0bea53785f0e499a86cd3c069de42680" },
+{name:" single showcase",  url:    "showcases/0bea53785f0e499a86cd3c069de42680" },
 {name:"portfolio",  url:   "p/7b7fb73e-1c4c-4e74-b470-ff14f363ad90?pageId=a67dddd3b1fc445ab94554fcf05ef2b2" },
+{name:"portfolio booking site",  url:   "p/roland" },
 { name:"notifications",  url:  "notifications?tab=personal" },
 { name:"notfications with tabs", url:   "notifications?tab=mimbboss" },
 {   name:"appointments ",  url:"appointments" },
@@ -14,7 +15,7 @@ export const deeplinks =[
 {   name:"policies", url: "page/policies" },
 {  name:"pay", url:  "page/pay" },
 {  name:"availability", url:  "availability" },
-{  name:"backstage", url:  "backstage" }
-{  name:"login", url:  "login" }
+{  name:"backstage", url:  "backstage" },
+{  name:"login", url:  "login" },
 {  name:"signup", url:  "signup" }
 ]
