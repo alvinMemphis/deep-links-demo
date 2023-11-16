8a99c69cf6c5c6dcc00e2307511b0f0e4aca13e4
alvinnsongambi9@gmail.com <alvinnsongambi9@gmail.com>
2023-11-16 18:36:43 +0300

    backstge changes

--- api/testlinks.js
+++ api/testlinks.js
@@ -8,14 +8,32 @@ export const deeplinks =[
 { name:"notfications with mimbboss", url:   "notifications?tab=mimboss" },
 {   name:"appointments  user",  url:"appointments" },
 {   name:"user apppt list view appointments ",  url:"appointments/id" },
-{   name:"appointments mimbboss ",  url:"/backstage/appointments" },
-{  name:"single appt mimbboss", url:  "backstage/appointments/0223ddf2615a43c0869ce4f46f43c924" },
-{  name:"single appt user", url:  "appointments/0223ddf2615a43c0869ce4f46f43c924" },
+{   name:"appointments mimbboss ",  url:"backstage/appointments" },
+{  name:"mimbboss appt", url:  "backstage/appointments/0223ddf2615a43c0869ce4f46f43c924" },
+{  name:"user is appt", url:  "appointments" },
+{  name:"single  appt user", url:  "appointments/0223ddf2615a43c0869ce4f46f43c924" },
 { name:"profile",  url:  "profile/7b7fb73e-1c4c-4e74-b470-ff14f363ad90" },
 {   name:"policies", url: "backstage/policies" },
 {  name:"pay", url:  "backstage/pay" },
-{  name:"availability", url:  "availability" },
+{  name:"availability", url:  "backstage/availability" },
 {  name:"backstage", url:  "backstage" },
+  {name: 'backstage',url:"backstage"},
+      {name: 'servicemenulist',url:"backstage/servicemenuList"},
+      {name: 'addons',url:"backstage/addson"},
+      {name: 'payment',url:"backstage/payment?page&paymentAccount&missingFields"},
+      {name: 'policies',url:"backstage/policies"},
+      {name: 'availabilitywelcome',url:"backstage/availabilityWelcome"},
+      {name: 'mimbboclasslist',url:"backstage/mimbboclasslist"},
 {  name:"login", url:  "login" },
 {  name:"signup", url:  "signup" }
 ]
+const drawerRoutes = [
+    // {
+    //   label: 'Home',
+    //   routeName: 'Home',
+    //   icon: 'home-outline',
+    //   screen: 'Home',
+    //   params: null,
+    // },
+   
+  ];
